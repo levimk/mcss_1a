c29a043eaf6bcee86b310d2dfff5a6d214268a53
Levi <levimckenzie93@gmail.com>
2021-03-29 12:30:32 +1100

    Organise constructors

--- sol_2/Sim.java
+++ sol_2/Sim.java
@@ -8,21 +8,22 @@ public class Sim {
      */
     public static void main(String[] args) {
         
-    	// Create system components
+    	// Components of the main carousel subsystem
         Carousel mainCarousel = new Carousel(Params.CAROUSEL_SIZE);
-
+        CarouselDrive mainDriver = new CarouselDrive(mainCarousel);
         Producer producer = new Producer(mainCarousel);
         Consumer distributor = new Consumer(mainCarousel);
 
+        // Components of the destroyer carousel subsystem
         Carousel destroyerCarousel = new Carousel(2);
         Consumer destroyer = new Consumer(destroyerCarousel);
+        CarouselDrive destroyerDriver = new CarouselDrive(destroyerCarousel);
 
+        // Components of the shuttle subsystem
         InspectionBay inspectionBay = new InspectionBay(destroyerCarousel);
         Shuttle shuttle = new Shuttle(mainCarousel, inspectionBay);
 
 
-        CarouselDrive mainDriver = new CarouselDrive(mainCarousel);
-        CarouselDrive destroyerDriver = new CarouselDrive(destroyerCarousel);
 
         // start threads
         distributor.start();
